1099df7e1c2fdde5d1aa179081dca46dd3f665d7
adictonator <adityabhaskarsharma@gmail.com>
2022-08-21 22:46:48 +0530

    naming fixed for layout

---
+++ components/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from './Footer'
+import Header from './Header'
+
+export default function Layout({ children }) {
+	return (
+		<>
+			{/*<Header />*/}
+			<main className="flex min-h-screen w-full flex-col">
+				{children}
+			</main>
+			{/*<Footer />*/}
+		</>
+	)
+}
