429410a3adcfe0605889fe02e495f389ea61c62c
adictonator <adityabhaskarsharma@gmail.com>
2022-08-22 15:12:35 +0530

    coming soon changes to staging

--- components/Sections/Hero.jsx
+++ components/Sections/Hero.jsx
@@ -1,37 +1,7 @@
 import React, { useState } from 'react'
-import anime from 'animejs'
 
 export default function Hero() {
 	const [fullNameDisplayed, setFullNameDisplayed] = useState(false)
-
-	const dummy = (e) => {
-		anime({
-			targets: e.target,
-			easing: 'easeOutCubic',
-			color: 'red',
-			elasticity: 250,
-			zIndex: 51,
-			opacity: 1,
-			translateX: {
-				value: 250,
-				duration: 800,
-			},
-			rotate: {
-				value: 360,
-				duration: 1800,
-				easing: 'easeInOutSine',
-			},
-			scale: {
-				value: 2,
-				duration: 1600,
-				delay: 800,
-				easing: 'easeInOutQuart',
-			},
-			delay: 250, // All properties except 'scale' inherit 250ms delay
-		})
-		//document.getElementById('skills')?.classList.remove('opacity-0')
-		//document.getElementById('skills')?.classList.add('z-50')
-	}
 
 	const showFullName = (e) => {
 		if (!fullNameDisplayed) {
@@ -61,25 +31,36 @@ export default function Hero() {
 	return (
 		<section className="z-50 flex h-screen min-h-full flex-col justify-center p-10 text-center">
 			<h1 className="font-lovelo text-9xl text-maroon drop-shadow-xl">
-				Hey! I am
-				<br />
-				<span
-					className="cursor-pointer font-lovelo-lb transition-all duration-300"
-					title="Want to click it?"
-					onClick={showFullName}
-				>
-					Aditya
-				</span>
+				Oh, no! <br /> You caught me.
 			</h1>
-			<h3 className="mt-10 font-checker text-4xl text-powder-blue">
-				Full stack dev, semi-pro gamer, keen learner.
-			</h3>
-			{/* show my full name on hover or a click maybe? for desktop only i guess */}
-			{/*add some easter eggs maybe? I like them.*/}
-			{/* A link to the next section */}
-			<a className="" onClick={(e) => dummy(e)}>
-				See What I Can Do
-			</a>
+			<span className="text-2xl text-biege">
+				I am still working on some stuff currently.
+				<br />
+				Maybe come back later and I might just have something ready to
+				showoff.
+			</span>
+			<span className="mt-3 text-lg italic text-biege">
+				I am not being{' '}
+				<a
+					href="https://lazycodelab.com"
+					className="text-powder-blue underline duration-300 hover:underline-offset-4"
+				>
+					Lazy
+				</a>
+			</span>
+
+			<div className="mx-auto mt-3 flex max-w-xs justify-between">
+				<a href="https://twitter.com/adictonator">
+					<svg
+						role="img"
+						viewBox="0 0 24 24"
+						xmlns="http://www.w3.org/2000/svg"
+						className="h-5 w-5 fill-current text-biege duration-300 hover:fill-[#1DA1F2]"
+					>
+						<path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
+					</svg>
+				</a>
+			</div>
 		</section>
 	)
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,70 +1,15 @@
 import React from 'react'
-import anime from 'animejs'
 import Head from 'next/head'
-import { useCallback, useEffect, useState } from 'react'
 import Layout from '../components/Layout'
-import ContactMe from '../components/Sections/ContactMe'
 import Hero from '../components/Sections/Hero'
-import Portfolio from '../components/Sections/Portfolio'
-import Skills from '../components/Sections/Skills'
 
 export default function Home() {
-	const showCard = (e) => {
-		document.getElementById(
-			'skills'
-		).style.transform = `translateX(500px) translateY(100%)`
-
-		anime({
-			targets: 'section#skills',
-			easing: 'easeOutElastic',
-			elasticity: 250,
-			duration: 800,
-			zIndex: 51,
-			translateY: 0,
-			translateX: 0,
-			//opacity: 1,
-			begin: function (anim) {
-				anim.animatables[0].target.classList.remove('hidden')
-				//anim.animatables[0].target.classList.add('absolute')
-			},
-		})
-	}
-
 	return (
 		<Layout>
 			<Head>
 				<title>Good to see you! - adictonator</title>
 			</Head>
-			{/*<SectionsNav />*/}
 			<Hero />
-			<h2
-				className="absolute bottom-28 z-50"
-				onClick={(e) => showCard(e)}
-			>
-				Maybe chcek heresss
-			</h2>
-			<Skills />
-			{/*<Portfolio />
-			<ContactMe />*/}
-
-			<div
-				className="type pointer-events-none absolute z-0"
-				data-type-transition
-				aria-hidden="true"
-			>
-				<div className="type__line">welcome welcome welcome</div>
-				<div className="type__line">full stack developer</div>
-				<div className="type__line">php js node php js node</div>
-				<div className="type__line">
-					konnichiwa konnichiwa konnichiwa
-				</div>
-				<div className="type__line">welcome welcome welcome</div>
-				<div className="type__line">full stack developer</div>
-				<div className="type__line">php js node php js node</div>
-				<div className="type__line">
-					konnichiwa konnichiwa konnichiwa
-				</div>
-			</div>
 		</Layout>
 	)
 }
