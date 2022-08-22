7f9aad7ae06e8fa11c29a5b368ada81f92b10ea4
adictonator <adityabhaskarsharma@gmail.com>
2022-08-22 15:21:43 +0530

    overflow hidden

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Layout({ children }) {
 	return (
 		<>
-			<main className="flex min-h-screen w-full flex-col">
+			<main className="flex min-h-screen w-full flex-col overflow-hidden">
 				{children}
 			</main>
 		</>
