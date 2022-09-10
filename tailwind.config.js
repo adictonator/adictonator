0f9b8ffffa127bf0495ad9a6adf9210084c059a5
adictonator <adityabhaskarsharma@gmail.com>
2022-09-10 19:02:53 +0530

    cleaned up tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,39 +5,20 @@ module.exports = {
 	],
 	theme: {
 		fontFamily: {
-			display: ['Pixel'],
 			checker: ["'Josefin Sans', sans-serif"],
 			lovelo: ['LoveloBlack, sans-serif'],
 			'lovelo-lb': ['LoveloLB, sans-serif'],
 			'lovelo-ll': ['LoveloLL, sans-serif'],
+			'fira-code': ['Fira Code Retina, monospace'],
 		},
 		extend: {
 			animation: {
 				'spin-slow': 'spin 30s linear infinite',
-				wave: 'wave 6s ease-in-out infinite',
-			},
-			keyframes: {
-				wave: {
-					'0%': {
-						transform: 'translateY(0) rotate(0deg)',
-					},
-					'100%': {
-						transform: 'translateY(-100%) rotate(400deg)',
-					},
-				},
 			},
 			colors: {
-				lol: '#123C69',
-			},
-			textColor: {
-				'brand-red': '#E63946',
-				'powder-blue': '#A8DADC',
 				maroon: '#AC3B61',
-				biege: '#BAB2B5',
-			},
-			backgroundColor: {
+				linen: '#EEE2DC',
 				'indigo-dye': '#123C69',
-				pb: '#1d3557',
 			},
 			backgroundImage: {
 				primary: "url('/images/bg.svg')",
