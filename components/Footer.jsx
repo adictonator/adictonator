e7b9b0bb429298a60700c6a10afd91a1043747b2
adictonator <adityabhaskarsharma@gmail.com>
2022-09-10 19:14:37 +0530

    updated footer area

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
 export default function Footer() {
-	return <footer>Footer</footer>
+	return (
+		<footer className="relative mx-auto max-w-xs py-3 text-sm text-linen/40 duration-300">
+			&copy; {new Date().getFullYear()}
+		</footer>
+	)
 }
